58eb5715335c7bb0e8e1d58eb05d1b4c163b8c17
nathanang2000 <nathanang2000@gmail.com>
2013-12-13 18:33:09 -0800

    Create English_To_pigLatin_Translator.py

---
+++ English_To_pigLatin_Translator.py
@@ -0,0 +1,15 @@
+pyg = 'ay'
+original = raw_input('Enter a word:')
+if str(original) > 0 and original.isalpha():
+    first = original[0]
+    original = original.lower()
+    word = original
+    new_word = word + pyg
+    print(word)
+    if first == "u" or first == "i" or first == "e" or first == "a" or first == "o":
+        print(new_word)
+    else:
+        new_word = original[1:] + original[0:1] + pyg
+        print(new_word)
+else:
+    print 'Error. Try again.'
